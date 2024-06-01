12dc3a9d587bc9ebbd6bcd674fc8f37a7034bd6a
Haripriya-VR <hpvr80343@gmail.com>
2024-06-01 13:18:31 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials:true
+}))
+
+const PORT = process.env.
+
+app.listen(PORT,()=>{
+    
+})
